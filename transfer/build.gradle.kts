927c322f2b33106c1632ddf4909e6cae12860a52
jon890 <jon89071@gmail.com>
2025-03-23 12:19:42 +0900

    :tada: Feat(discovery) 디스커버리 서버 세팅

--- transfer/build.gradle.kts
+++ transfer/build.gradle.kts
@@ -9,15 +9,18 @@ allOpen {
 
 dependencies {
     implementation(libs.spring.boot.web)
-
     implementation(libs.spring.kafka)
-
     implementation(libs.spring.data.jpa)
     implementation(libs.mysql.connector.j)
-
     implementation(libs.flyway.mysql)
     implementation(libs.flyway.core)
+    
+    // WebClient 의존성 추가
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-    implementation(platform(libs.spring.cloud))
-    implementation(libs.spring.cloud.openfeign)
+    // Spring Cloud 의존성 추가
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.0"))
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
 }
